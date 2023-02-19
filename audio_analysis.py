d1e1f9c4a0c2056b49319fddd16f98a190875ff9
Evan Gerritz <evgerritz@gmail.com>
2023-02-19 15:55:28 -0500

    timbrel analysis, modify by adding

--- audio_analysis.py
+++ audio_analysis.py
@@ -7,6 +7,12 @@ from scipy.fftpack import fft
 import numpy as np
 import scipy.stats as st
 
+def smooth_data(x):
+    # smooth to remove vibrato in get_freq, or just remove noise
+    # using gaussian kernel with sigma=2
+    kern_domain = np.linspace(-2, 2, 21+1)
+    gaussian_kernel = st.norm.pdf(kern_domain)
+    return np.convolve(x, gaussian_kernel, mode='same')
 
 def get_freq(freq, samplerate, samples, smooth=True):
     # good value empirically for maximizing number of freqs and times
@@ -14,13 +20,9 @@ def get_freq(freq, samplerate, samples, smooth=True):
     f, t, Pxx = signal.spectrogram(samples,samplerate,signal.get_window('hamming',int(num_samples)))
     frequency_index = np.argmin(np.abs(f-freq))
     amps = Pxx[frequency_index,:]
-    
-    # smooth to remove vibrato
-    # using gaussian kernel with sigma=2
+
     if smooth:
-        kern_domain = np.linspace(-2, 2, 21+1)
-        gaussian_kernel = st.norm.pdf(kern_domain)
-        amps = np.convolve(amps, gaussian_kernel, mode='same')
+        amps = smooth_data(amps)
     return t, amps
 
 def plot_amp(freq, samplerate, samples, smooth=True):
@@ -52,6 +54,12 @@ def plot_amps(fund_freq, samplerate, samples, smooth=True):
                         hspace=1.0)
     plt.show()
 
+def print_array(arr):
+    print('[', end = '')
+    for x in arr:
+        print(str(round(x, 5)) + ', ', end='')
+    print('], ',end='')
+
 def get_envs(fund_freq, samplerate, samples, smooth=True):
     """
     prints out a syntactic supercollider array for specifying
@@ -62,21 +70,61 @@ def get_envs(fund_freq, samplerate, samples, smooth=True):
     for i in range(30):
         t, amps = get_freq(freq, samplerate, samples, smooth)
         amps = amps/np.max(amps) # normalize to 0 to 1
-        print('[', end = '')
-        for amp in amps:
-            print(str(round(amp, 5)) + ', ', end='')
-        print('], ',end='')
+        print_array(amps)
         freq += fund_freq
     print(']')
 
+def timbrel_change(smooth = True, print_out = False):
+    # these values obtained by isolating a quiet/loud segment of a solo violin performance
+    # running audacity's spectral analysis to get amplitudes for various frequencies
+    # and taking the first 30 partials after finding the fundamental frequency
+    loud_partial_amps = np.array([-51.93972 , -49.4104  , -46.828625, -56.960018, -64.312508,
+       -62.737423, -29.59796 , -62.45232 , -64.916168, -64.447052,
+       -70.183189, -72.147018, -65.468178, -70.745598, -59.947681,
+       -59.926273, -69.282608, -74.263039, -59.553307, -63.807056,
+       -94.537758, -62.298313, -78.946373, -74.343536, -64.548203,
+       -82.339119, -84.327194, -70.587006, -74.463646, -83.49617 ])
+    quiet_partial_amps = np.array([ -65.825394,  -34.847195,  -52.882778,  -49.364624,  -54.84634 ,
+        -51.980816,  -59.094482,  -48.69043 ,  -66.581573,  -54.611156,
+        -74.646492,  -59.795284,  -81.939926,  -80.814651,  -77.027634,
+        -76.393929,  -89.03508 ,  -72.259262,  -72.2481  ,  -74.552094,
+       -106.836113,  -81.866112,  -94.832108,  -79.970268,  -83.187508,
+        -89.181671,  -98.206154,  -94.213165,  -98.791794, -100.895943])
+
+    if smooth:
+        loud_partial_amps = smooth_data(loud_partial_amps)
+        quiet_partial_amps = smooth_data(quiet_partial_amps)
+
+    # plot data
+    fig,axs = plt.subplots(3,1)
+    axs[0].plot(loud_partial_amps)
+    axs[0].set_title("Amplitude vs Partial No for Loud Violin")
+    axs[1].plot(quiet_partial_amps)
+    axs[1].set_title("Amplitude vs Partial No for Quiet Violin")
+    diff = loud_partial_amps - quiet_partial_amps
+    axs[2].plot(diff)
+    axs[2].set_title("Difference between Loud and Quiet Violin Partials")
+    plt.show()
+
+    if print_out:
+        print_array(diff)
 
 
-#samplerate, samples = wavfile.read('./string_vln.b3.wav')
-#b3 = 247
-#plot_amps(b3, samplerate, samples)
-#get_envs(b3, samplerate, samples, smooth=False)
 
-samplerate, samples = wavfile.read('./string_vln.g3.wav')
-g3 = 196
-plot_amps(g3, samplerate, samples, smooth=True)
-get_envs(g3, samplerate, samples, smooth=True)
+
+if __name__ == '__main__':
+    # uncomment these lines to plot/get envelope data for the b3 violin file
+    samplerate, samples = wavfile.read('./string_vln.b3.wav')
+    b3 = 247
+    plot_amps(b3, samplerate, samples, smooth=False)
+    get_envs(b3, samplerate, samples)
+
+    # uncomment these lines to plot/get envelope data for the g3 violin file
+    #samplerate, samples = wavfile.read('./string_vln.g3.wav')
+    #g3 = 196
+    #plot_amps(g3, samplerate, samples, smooth=False)
+    #get_envs(g3, samplerate, samples, smooth=True)
+
+    # uncomment these lines to plot timbrel change for two amplitudes
+    #timbrel_change(smooth=False)
+    #timbrel_change(print_out=True)
